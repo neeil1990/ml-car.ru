170600a8294995fa3ff4d4b77d4f51f8139c2970
neeil1990 <neeil@mail.ru>
2021-04-13 17:02:01 +0300

    #116322 detail promo slider

--- bitrix/templates/ml/components/bitrix/news/service/detail.php
+++ bitrix/templates/ml/components/bitrix/news/service/detail.php
@@ -14,55 +14,18 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 
-$arFilter = array(
-    "IBLOCK_ID" => $arParams["IBLOCK_ID"],
-    "ACTIVE" => "Y",
-    "GLOBAL_ACTIVE" => "Y",
-);
-if (0 < intval($arResult["VARIABLES"]["SECTION_ID"]))
-    $arFilter["ID"] = $arResult["VARIABLES"]["SECTION_ID"];
-elseif ('' != $arResult["VARIABLES"]["SECTION_CODE"])
-    $arFilter["=CODE"] = $arResult["VARIABLES"]["SECTION_CODE"];
-
-$obCache = new CPHPCache();
-if ($obCache->InitCache(36000, serialize($arFilter), "/iblock/catalog"))
-{
-    $arCurSection = $obCache->GetVars();
-}
-elseif ($obCache->StartDataCache())
-{
-    $arCurSection = array();
-    if (Loader::includeModule("iblock"))
-    {
-        $dbRes = CIBlockSection::GetList(array(), $arFilter, false, array("ID", "NAME", "UF_PROMO"));
-
-        if(defined("BX_COMP_MANAGED_CACHE"))
-        {
-            global $CACHE_MANAGER;
-            $CACHE_MANAGER->StartTagCache("/iblock/catalog");
-
-            if ($arCurSection = $dbRes->Fetch())
-                $CACHE_MANAGER->RegisterTag("iblock_id_".$arParams["IBLOCK_ID"]);
-
-            $CACHE_MANAGER->EndTagCache();
-        }
-        else
-        {
-            if(!$arCurSection = $dbRes->Fetch())
-                $arCurSection = array();
-        }
-    }
-    $obCache->EndDataCache($arCurSection);
-}
-if (!isset($arCurSection))
-    $arCurSection = array();
+$arSelect = Array("ID", "NAME", "PROPERTY_PROMO");
+$arFilter = Array("IBLOCK_ID" => $arParams['IBLOCK_ID'], "CODE" => $arResult['VARIABLES']['ELEMENT_CODE']);
+$res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+if($ob = $res->GetNextElement())
+    $arFields = $ob->GetFields();
 ?>
 
-<? if($arCurSection['UF_PROMO']): ?>
+<? if($arFields['PROPERTY_PROMO_VALUE']): ?>
     <div class="main-index">
         <div class="wrapper clearfix">
             <?
-            $APPLICATION->IncludeFile(SITE_TEMPLATE_PATH."/include/services_slider.php", Array("DESCRIPTION" => $arCurSection['UF_PROMO']), Array(
+            $APPLICATION->IncludeFile(SITE_TEMPLATE_PATH."/include/services_slider.php", Array("DESCRIPTION" => $arFields['PROPERTY_PROMO_VALUE']), Array(
                 "MODE"      => "php",
                 "NAME"      => "",
                 "TEMPLATE"  => "",
